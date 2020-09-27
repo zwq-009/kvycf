a43245070ed26cb152ca69ec89828c8f06119fec
房镇 <2640347015@qq.com>
2020-09-27 14:49:20 +0800

    2020年9月27日14:49:11

---
+++ src/components/Service/shouye/shouye.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="page">
+    <div class="box">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page{
+  width: 1100px;
+  height: 100%;
+}
+. box box1{
+  width: 300px;
+  height: 80px;
+  position: fixed;
+  top: 20px;
+  left: 400px;
+}
+.box box2{
+  width: 150px;
+  height: 150px;
+  position: fixed;
+  top: 120px;
+  left: 100px;
+}
+.box box3{
+  width: 150px;
+  height: 150px;
+  position: fixed;
+  top: 120px;
+  left: 350px;
+}
+.box box4{
+  width: 150px;
+  height: 150px;
+  position: fixed;
+  top: 120px;
+  left: 600px;
+}
+.box box5{
+  width: 150px;
+  height: 150px;
+  position: fixed;
+  top: 120px;
+  left: 850px;
+}
+</style>
