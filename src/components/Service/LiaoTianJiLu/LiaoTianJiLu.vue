cfb68b4473099402dfa91d97ec36b6349e754724
房镇 <2640347015@qq.com>
2020-09-27 20:20:42 +0800

    2020年9月27日20:20:37

--- src/components/Service/LiaoTianJiLu/LiaoTianJiLu.vue
+++ src/components/Service/LiaoTianJiLu/LiaoTianJiLu.vue
@@ -1,7 +1,36 @@
-<template> </template>
+<template>
+    <div class="page3">聊天目录
+    <div class="box5">
+        <div class="el-icon-iconName">
+        <el-button type="primary">搜索<i class="el-icon-search el-icon--right"></i></el-button>
+    </div>
+    </div>
+    <div class="box6"></div>
+    </div>
+</template>
 
 <script>
 export default {}
 </script>
 
-<style></style>
+<style>
+.page3 {
+  width: 1500px;
+  height: 1100px;
+  background-color: blue;
+  font-size: 3em;
+}
+.box5{
+    position: absolute;
+    top: 10px;
+    right: 50px;
+}
+.box6{
+    width: 1450px;
+    height: 1000px;
+    position: absolute;
+    top: 80px;
+    left: 25px;
+    background-color: hotpink;
+}
+</style>
---
+++ src/components/Service/LiaoTianJiLu/LiaoTianJiLu2.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="page4">聊天记录
+      <div class="box7"></div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.page4{
+  width: 1500px;
+  height: 1100px;
+  background-color: greenyellow;
+  font-size: 2em;
+}
+.box7{
+    width: 1450px;
+    height: 800px;
+    position: fixed;
+    top: 60px;
+    left: 25px;
+    background-color: brown;
+}
+</style>
