e2b07a148bf975674be8831434c9624bb6a1de1a
张文强 <1097234514@qq.com>
2020-09-28 14:04:18 +0800

    边框图标与表头。

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,64 +1,46 @@
 <template> 
 <el-container style="height: 707px; width: 866px;">
-  <el-aside>
+  <aside class="el-aside" style="overflow:unset">
     <div class="logo"><div class="img">Logo</div></div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-home" ></use>
-            </svg>
+            <i id="icon" class="el-icon-s-home"></i>
             <div class="tip">首页</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-comment"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-comment"></i>
             <div class="tip">会话</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-custom"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-custom"></i>
             <div class="tip">访客</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-unfold"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-unfold"></i>
             <div class="tip">历史</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" ariel-icon-usera-hidden="true">
-                <use xlink:href="#icon-wode"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-custom"></i>
             <div class="tip">客户</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-order"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-order"></i>
             <div class="tip">工单</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-marketing"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-marketing"></i>
             <div class="tip">统计</div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-s-tools"></use>
-            </svg>
+            <i id="icon" class="el-icon-s-tools"></i>
             <div class="tip"> 设置 </div>
         </div>
         <div class="nav-item">
-            <svg class="icon" aria-hidden="true">
-                <use xlink:href="el-icon-switch-button"></use>
-            </svg>
+            <i id="icon" class="el-icon-switch-button"></i>
             <div class="tip">退出</div>
         </div>
-  </el-aside>
+  </aside>
   
-    <el-header style="text-align: right; font-size: 12px">
+<header class="el-header">
   <div class="customer-info">
         <div class="head">
             <img src="u94.svg" alt="" class="header">
@@ -70,7 +52,7 @@
             <li class="statu">离线</li>
         </div>
     </div>
-    </el-header>
+    </header>
     
     <el-main>
       <div :is="currentView"></div>
@@ -100,53 +82,48 @@ export default {
 
 <style>
   .el-aside {
-    width: 64px;
-    background-color: rgb(173, 182, 235);
-    color: #333;
-    height: 700px;
-    position: fixed;
-    left: 0;
-    top: 0;
-    color: #fff;
+  width: 64px;
+  height: 715px;
+  background-color: rgba(23, 28, 58, 1);
+
   }
 .logo{
+    margin-left: 8px;
+    border-width: 0px;
+    position: absolute;
+    left: 0px;
+    top: 10px;
     width: 48px;
     height: 48px;
+  }
+  .img{
+   width: 48px;
+    height: 48px;
     border-radius: 50%;
+
     background-color: #006eff;
     color: #fff;
+
     text-align: center;
+    line-height: 48px;
+
     font-family: 'Arial Regular', 'Arial', sans-serif;
     font-size: 14px;
     font-weight: 700;
-    position: absolute;
-    align-self: center;
-    padding: 2px 2px 2px 2px;
-    box-sizing: border-box;
-    width: 100%;
-  }
-  .img{
-    width: 48px;
-    height: 48px;
-    margin: 29px 8px;
-    background-color: #006eff;
-    border-radius: 50%;
-    text-align: center;
-    line-height: 48px;
-    font-size: 12px;
-    font-weight: 800;
 }
-  .icon {
-    font-size: 21px;
-    width: 1.5em;
-    height: 1.5em;
-    vertical-align: -0.15em;
-    fill: currentColor;
-    overflow: hidden;
+  i{
+    border-width: 0px;
+    left: 22px;
+    top: 116px;
+    width: 25px;
+    height: 22px;
+    display: flex;
 }
 .nav-item {
-  width: 60px;
-  height: 56px;
+    left: 0px;
+    top: 60px;
+    width: 64px;
+    height: 56px;
   font-size: 18px;
   font-weight: 1200;
   text-align: center;
@@ -156,21 +133,22 @@ export default {
   position: relative;
 }
 .tip{
-    display: none;
-    background-color:  #a59b9b;
-    width: 40px;
-    height: 30px;
-    border-radius: 4px;
     position: absolute;
-    left: 76px;
-    top: 6px;
-    text-align: center;
+    left: 72px;
+    top: 13px;
+    height: 30px;
+    width: 36px;
+    border-radius: 2px;
+
+    background: #333;
+    font-size: 10px;
     line-height: 30px;
-    color: rgb(39, 138, 39);
-    font-size: 14px;
+
+    display: none;
 }
 .nav-item:hover .tip{
     display: block;
+
 }
 .tip::before{
     content: '';
@@ -185,11 +163,13 @@ export default {
 }
 
 .el-header {
-    background-color: #B3C0D1;
+    background-color: #e1e6ee;
     color: #333;
     line-height: 60px;
-    width: 800px;
+    width: 743px;
     height: 64px;
+    text-align: right;
+    font-size: 12px;
   }
   .customer-info{
             margin:0;
@@ -197,8 +177,9 @@ export default {
             width: 157px;
             height: 0;
             float: right;
+            right: 0;
             }
-  .customer-info .head{
+  .head{
     border-width: 0px;
     position: absolute;
     right: 0px;
@@ -206,11 +187,12 @@ export default {
     width: 130px;
     height: 64px;
     line-height: 64px;
-    background-color: #fff ;
+    background-color: rgb(225, 234, 235) ;
     border: none;
     border-radius: 2px;
         }
-    .head .customer-name, .head .tip{
+    .header .tip{
+     font-size: 15px;
       position: relative;
       top: -16px;
       }
@@ -219,7 +201,7 @@ export default {
     background-color: #fff;
     position:absolute;
     width:120px;
-    top:45px;
+    top:65px;
     padding-right: 10px;
     right: 0;
 }
