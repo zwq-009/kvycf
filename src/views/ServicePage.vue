b7da0aff582ea0c635f1e4fd44da2f9037af6442
张文强 <1097234514@qq.com>
2020-09-28 19:23:36 +0800

    边栏与标头。

--- src/views/ServicePage.vue
+++ src/views/ServicePage.vue
@@ -1,65 +1,67 @@
 <template> 
 <el-container class="ServicePage_el-container">
   <el-aside class="ServicePage_el-aside"  style="overflow:unset">
-    <div class="logo"><div class="img">Logo</div></div>
-        <div class="nav-item">
+    <div class="ServicePage_logo">
+        <div class="ServicePage_img">Logo</div>
+        </div>
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-home"></i>
-            <div class="tip">首页</div>
+            <div class="ServicePage_tip">首页</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-comment"></i>
-            <div class="tip">会话</div>
+            <div class="ServicePage_tip">会话</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-custom"></i>
-            <div class="tip">访客</div>
+            <div class="ServicePage_tip">访客</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-unfold"></i>
-            <div class="tip">历史</div>
+            <div class="ServicePage_tip">历史</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-custom"></i>
-            <div class="tip">客户</div>
+            <div class="ServicePage_tip">客户</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-order"></i>
-            <div class="tip">工单</div>
+            <div class="ServicePage_tip">工单</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-marketing"></i>
-            <div class="tip">统计</div>
+            <div class="ServicePage_tip">统计</div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-s-tools"></i>
-            <div class="tip"> 设置 </div>
+            <div class="ServicePage_tip"> 设置 </div>
         </div>
-        <div class="nav-item">
+        <div class="ServicePage_nav-item">
             <i id="icon" class="el-icon-switch-button"></i>
-            <div class="tip">退出</div>
+            <div class="ServicePage_tip">退出</div>
         </div>
   </el-aside>
   
 <el-header class="ServicePage_el-header">
-  <div class="customer-info">
-        <div class="head">
-            <img src="u94.svg" alt="" class="hop">
+  <div class="ServicePage_customer-info">
+        <div class="ServicePage_head">
+            <img src="u94.svg" class="ServicePage_hop">
             <span class="customer-name">凯文客服</span>
-            <img src="u93.svg" alt="" class="tip">
+            <img src="u93.svg" class="ServicePage_tip">
         </div>
-        <div class="status">
-            <li class="statu">在线</li>
-            <li class="statu">离线</li>
+        <div class="ServicePage_status">
+            <li class="ServicePage_statu">在线</li>
+            <li class="ServicePage_statu">离线</li>
         </div>
     </div>
     </el-header>
-    <el-main class="mainen"  >
+    <el-main class="ServicePage_main"  style="overflow:unset">
+
         <router-view></router-view>
-        <!-- <div :is="currentView"></div> -->
+
     </el-main>
 </el-container>
 </template>
-
 <script>
 import CompanyPage from '@/components/Enter/Company/CompanyPage.vue'
 import CompanyPageR1 from '@/components/Enter/Company/CompanyPageR1.vue'
@@ -82,29 +84,29 @@ export default {
 
 <style>
 .ServicePage_el-container{
-height: 707px;
-width: 866px;
-}
+    height: 1200px;
+    width:1500px;
+    }
   .ServicePage_el-aside {
   width: 64px!important;
-  height: 715px;
-  background-color: rgba(23, 28, 58, 1);
+  height: 1200px;
+  background-color: rgb(106, 126, 236);
+  text-align: center;
+  position: absolute;
   }
-.logo{
-    margin-left: 8px;
-    border-width: 0px;
-    position: absolute;
-    left: 0px;
-    top: 10px;
-    width: 48px;
-    height: 48px;
+.ServicePage_logo{
+    text-align:center;
+    width: 64px;
+    height: 64px;
+    background-color: rgb(106, 126, 236);
   }
-  .img{
+  .ServicePage_img{
+      margin-left: 8px;
    width: 48px;
     height: 48px;
     border-radius: 50%;
 
-    background-color: #006eff;
+    background-color: #c69ce2;
     color: #fff;
 
     text-align: center;
@@ -122,9 +124,9 @@ width: 866px;
     height: 22px;
     display: flex;
 }
-.nav-item {
+.ServicePage_nav-item {
     left: 0px;
-    top: 60px;
+    top: 0px;
     width: 64px;
     height: 56px;
   font-size: 18px;
@@ -132,10 +134,9 @@ width: 866px;
   text-align: center;
   line-height: 56px;
   color: #fff;
-
   position: relative;
 }
-.tip{
+.ServicePage_tip{
     position: absolute;
     left: 72px;
     top: 13px;
@@ -149,11 +150,11 @@ width: 866px;
 
     display: none;
 }
-.nav-item:hover .tip{
+.ServicePage_nav-item:hover .ServicePage_tip{
     display: block;
 
 }
-.tip::before{
+.ServicePage_tip::before{
     content: '';
     width: 0;
     height: 0;
@@ -166,58 +167,65 @@ width: 866px;
 }
   
   .ServicePage_el-header {
-    background-color: #bcd1f3;
-    color: #333;
+      width: 100%;
+      height: 64px;
+      margin-left: 64px;
+      padding-right: 150px;
+      background-color: #bcd1f3;
+      color: #333;
     line-height: 60px;
-    width: 743px;
-    height: 64px;
     text-align: right;
     font-size: 12px;
-    position: fixed;
     top: 0px;
-    left: 64px;
+      position: absolute;
+
   }
-  .customer-info{
+  .ServicePage_customer-info{
             margin:0;
             position: relative;
             width: 157px;
-            height:64px;
+            height:60px;
             float: right;
             right: 0;
             }
-  .head{
+  .ServicePage_head{
+      margin-right: 64px;
     border-width: 0px;
     position: absolute;
     right: 0px;
     top: 0px;
     width: 130px;
-    height: 64px;
+    height: 60px;
     line-height: 64px;
     background-color: rgb(225, 234, 235) ;
     border: none;
     border-radius: 2px;
         }
-    .hop .tip{
+    .hop .ServicePage_tip{
      font-size: 15px;
       position: relative;
       top: -16px;
       }
-      .status{
+      .ServicePage_status{
     display:none;
     background-color: #fff;
     position:absolute;
     width:120px;
-    top:65px;
+    margin: 64px;
     padding-right: 10px;
     right: 0;
+    z-index:999
     }
-    .customer-info:hover .status{
+    .ServicePage_customer-info:hover .ServicePage_status{
        display: block;
     }
 
 
-.mainen{
-    background-color: rgb(160, 206, 233);
+.ServicePage_main{
+    background-color: rgb(180, 212, 231);
+    width:1500px!important;
+    height:1200px!important;
+    overflow: hidden;
 }
 
 
